10c8df9fd86715a357a1120b7b3a33e244f78aad
Aliaksandr Astapchyk <alastapchyk@gmail.com>
2024-04-20 15:42:50 +0200

    Register user and open app

--- tg-bot/src/commands/start.ts
+++ tg-bot/src/commands/start.ts
@@ -1,20 +1,50 @@
-import TelegramBot from "node-telegram-bot-api";
+import axios from "axios";
+import TelegramBot, { InlineKeyboardButton } from "node-telegram-bot-api";
 
-const rolesKeyboard = {
-  inline_keyboard: [
-    [
-      { text: "Rekruter", callback_data: "rekruter" },
-      { text: "Pracownik", callback_data: "pracownik" },
-    ],
-  ],
-};
+const MINI_APP_URL = process.env.MINI_APP_URL ?? "";
+const REGISTER_PATH = process.env.REGISTER_PATH ?? "";
+
+// const rolesKeyboard = {
+//   inline_keyboard: [
+//     [
+//       { text: "Rekruter", callback_data: "rekruter" },
+//       { text: "Pracownik", callback_data: "pracownik" },
+//     ],
+//   ],
+// };
 
 export function setStartCommand(bot: TelegramBot) {
   bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
 
-    bot.sendMessage(chatId, "Witamy na platformie do wyszukiwania pracy!\n\nWybierz rolę:", {
-      reply_markup: rolesKeyboard,
+    let token = "";
+
+    try {
+      const response = await axios.post(MINI_APP_URL + REGISTER_PATH, {
+        telegramId: chatId,
+        first_name: msg.from?.first_name,
+        username: msg.from?.username,
+        last_name: msg.from?.last_name,
+      });
+      token = response.data;
+      console.log("POST request successful");
+    } catch (error) {
+      console.error("Error making POST request");
+      await bot.sendMessage(chatId, "Nie udało się zalogować się :(");
+      return;
+    }
+
+    const urlWithParams = `${MINI_APP_URL}?token=${token}`;
+
+    const button: InlineKeyboardButton = {
+      text: "Otwórz aplikację",
+      web_app: { url: urlWithParams ?? "" },
+    };
+
+    bot.sendMessage(chatId, "Witamy na platformie do wyszukiwania pracy!", {
+      reply_markup: {
+        inline_keyboard: [[button]],
+      },
     });
   });
 
