219f50b3f7d3a9a6576d50524b0adc9d446b9173
Roman <bekrenov.roman@gmail.com>
2024-04-20 16:18:43 +0200

    Setup basic database tables; Create TelegramUser entity and necessary enums for data

---
+++ backend/src/main/java/com/lordsofcookies/telegramrecruiter/enums/Position.java
@@ -0,0 +1,12 @@
+package com.lordsofcookies.telegramrecruiter.enums;
+
+public enum Position {
+    FRONTEND_DEV,
+    BACKEND_DEV,
+    FULLSTACK_DEV,
+    PROJECT_MANAGER,
+    DEVOPS,
+    TESTER,
+    TEAM_LEAD,
+    TECH_LEAD;
+}
---
+++ backend/src/main/java/com/lordsofcookies/telegramrecruiter/enums/Technology.java
@@ -0,0 +1,14 @@
+package com.lordsofcookies.telegramrecruiter.enums;
+
+public enum Technology {
+    JAVA_SCRIPT,
+    TYPE_SCRIPT,
+    JAVA,
+    C_PLUS_PLUS,
+    C_SHARP,
+    ANGULAR,
+    REACT,
+    EMBEDDED,
+    RUBY,
+    VUE;
+}
