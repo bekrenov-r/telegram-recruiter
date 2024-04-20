eb1ecb5be1d2e28712973c596a9632ed602346bd
Roman <bekrenov.roman@gmail.com>
2024-04-20 13:23:26 +0200

    Init backend project

---
+++ backend/src/main/java/com/lordsofcookies/telegramrecruiter/TelegramrecruiterApplication.java
@@ -0,0 +1,13 @@
+package com.lordsofcookies.telegramrecruiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramrecruiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramrecruiterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lordsofcookies/telegramrecruiter/TelegramrecruiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lordsofcookies.telegramrecruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramrecruiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
